ae936973c24efe657722bd060856821fc00d8301
tt59317 <57324886+tt59317@users.noreply.github.com>
2021-08-29 20:34:25 +0300

    setSState

--- src/component/letsClick.js
+++ src/component/letsClick.js
@@ -2,13 +2,42 @@ import React, { Component } from 'react';
 
 class LetsClick extends Component {
     state = { 
-        num:45
+        num:0
      }
+    
+    // addCount = () => {
+    //     let {num} = this.state;
+    //     this.setState({num: ++num})
+    // }
+    // reduceCount = () => {
+    //     let {num} = this.state;
+    //     if(num > 0){
+    //     num--
+    //     this.setState({num})
+    //     }
+    // }
+    handleButtond = (action) => {
+        let {num} = this.state;
+
+        if(action === 'add'){
+            this.setState({num: ++num})
+        }
+        if(action === 'reduce' && num > 0){
+            this.setState({num: --num})
+        }
+        if(action === 'reset'){
+            this.setState({num :0})
+        }
+
+    }
+        
     render() { 
         return ( 
             <div style={{padding: 100}}>
+                <button onClick = {() => this.handleButtond('reset')}>Rest</button>
                 <h2 style = {{fontSize:'4rem',color:'midnightblue'}}>{this.state.num}</h2>
-                <button
+                {/* <button
+                    onClick = {this.addCount}
                     type="button"
                     className="btn btn-success"
                     style={{margin: 50,padding: 40,fontSize:'2.5rem'}}
@@ -17,6 +46,24 @@ class LetsClick extends Component {
                 </button>
                 <span> |--------------------------------| </span>
                 <button
+                    onClick = {this.reduceCount}
+                    type="button"
+                    className="btn btn-danger"
+                    style={{margin: 50,padding: 40,fontSize:'2.5rem'}}
+                >
+                     - 
+                </button> */}
+                <button
+                    onClick = {() =>this.handleButtond('add')}
+                    type="button"
+                    className="btn btn-success"
+                    style={{margin: 50,padding: 40,fontSize:'2.5rem'}}
+                >
+                    +
+                </button>
+                <span> |--------------------------------| </span>
+                <button
+                    onClick = {() =>this.handleButtond('reduce')}
                     type="button"
                     className="btn btn-danger"
                     style={{margin: 50,padding: 40,fontSize:'2.5rem'}}
